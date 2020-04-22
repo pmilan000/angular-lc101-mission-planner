7da7cc93a398644688d6f8135920ac9f5cf0672b
Milani <PMilan000@cable.comcast.com>
2020-04-22 00:20:01 -0400

    BonusPart

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-   experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
-
+   experiments: object[] = [
+     {name:'Moon soil sample', number: 1}, 
+     {name: 'Plant growth in orbit', number: 2},
+     {name:'Human bone density changes', number: 3},
+     {name:'Water droplet podcast for grades K-5', number: 4},
+     {name:'Satellite launch', number: 5}
+   ];
+   laboratory: object[] = [];
+   labProjects: number = 0;
+   maxItems: number = 3;
    constructor() { }
 
    ngOnInit() { }
 
+   addProjects(item: object) : boolean {
+    this.laboratory.push(item);
+    this.labProjects++;
+    return this.laboratory;
+  }
 }
